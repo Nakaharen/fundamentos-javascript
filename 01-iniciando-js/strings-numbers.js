c6058eb64bca817627d31cad1ea7b9a410eeebb7
Karen Nakahara <karenakahara@gmail.com>
2021-05-29 20:24:01 -0300

    strings and numbers

---
+++ 01-iniciando-js/strings-numbers.js
@@ -0,0 +1,13 @@
+let salarioDoAmigo = 1000
+let meuSalario = "2999.19"
+let meuSalarioCorrigido = Number (meuSalario)
+
+console.log(salarioDoAmigo + meuSalarioCorrigido)
+
+let minhaString = "Olá mundo!"
+let minhaOutraString = "Olá mundo 2"
+let minhaStringComVariaveis = `${minhaString} - ${minhaOutraString}, AE!`
+
+console.log(
+  minhaStringComVariaveis
+)
---
+++ 01-iniciando-js/strings-numbers.md
@@ -0,0 +1,5 @@
+## String
+É um texto criado com aspas simples, aspas duplas, ou crase (pode ser um número).
+
+## Number
+Sem aspas, pode ser números inteiros ou decimais.
