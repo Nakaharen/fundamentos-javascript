36929433bc4c5ddcf87c4141a2ec0b8730b1d74e
Karen Nakahara <karenakahara@gmail.com>
2021-06-08 22:44:16 -0300

    objetos globais

---
+++ 01-iniciando-js/objetos-globais.js
@@ -0,0 +1,7 @@
+console.log('keys', Object.keys({nome: 'Karen'}))
+console.log('values', Object.values({nome:'Karen'}))
+console.log('now', Date.now())
+console.log('random', Math.random())
+
+// global
+console.log('global', globalThis)
